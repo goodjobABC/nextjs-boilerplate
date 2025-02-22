805e9d18d51b4db9e183c2ff686a20b69dd2577b
goodjobABC <13718131196@163.com>
2025-02-22 21:55:33 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,13 +16,13 @@ export default function Home() {
           <li className="mb-2">
             这里是ShanShanstdio的导航页面
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>开始浏览页面吧！！！</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://shanziyi.top"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -37,7 +37,7 @@ export default function Home() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://shanziyi.top"
+            href="https://api.shanziyi.top"
             target="_blank"
             rel="noopener noreferrer"
           >
