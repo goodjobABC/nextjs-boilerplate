a5440ff3cc0e8400d4eae533ea108bf89a96d051
goodjobABC <13718131196@163.com>
2025-02-22 21:52:20 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
+            这里是ShanShanstdio的导航页面
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
@@ -37,22 +33,22 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+             现在前往
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://shanziyi.top"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+           获取API|免费接入|提交代码
           </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://api.shanziyi.top"
           target="_blank"
           rel="noopener noreferrer"
         >
